f68935916bdddb0e45586732642252ca3f90c622
Ferdinan Aurifio <ferdinan.aurifio@gmail.com>
2025-02-03 14:45:29 +0700

    add leafmap

--- latpypack/latpypack.py
+++ latpypack/latpypack.py
@@ -1,9 +1,38 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add_control(ipyleaflet.LayersControl())
+
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add(layer)
+        return layer
+    
+    def add_basemap(self, name, **kwargs):
+        """
+        Adds a basemap to the current map.
+
+        Args:
+            name (str or object): The name of the basemap as a string, or an object representing the basemap.
+
+        Raises:
+            TypeError: If the name is neither a string nor an object representing a basemap.
+
+        Returns:
+            None
+        """
+
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)
+
+
+
